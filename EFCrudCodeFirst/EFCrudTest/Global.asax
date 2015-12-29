a34318e1b61ea618d006c881497da439f22ff8e4
Jack Rola <jrola@precisiondesignstudios.com>
2015-12-29 14:11:12 -0500

    First Commit

---
+++ EFCrudCodeFirst/EFCrudTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFCrudTest.MvcApplication" Language="C#" %>
---
+++ EFCrudCodeFirst/EFCrudTest/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace EFCrudTest.Models
+{
+    public class Contact
+    {
+        [Required]
+        public Int32 ID { get; set; }
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string City { get; set; }
+
+    }
+
+    public class DBContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
